e3fc0d61e7e07ba171ca3c8315aa33162adde4a6
agomcam <agomcam813@iescarrillo.es>
2024-09-26 09:35:11 +0200

    Añadido funcionalidad a la app

--- app/src/main/java/com/example/myfirstappagc/SecondActivity.java
+++ app/src/main/java/com/example/myfirstappagc/SecondActivity.java
@@ -1,6 +1,11 @@
 package com.example.myfirstappagc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,35 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Obtenemos los componentes del Activity
+        EditText editText = findViewById(R.id.etText);
+        EditText editNumber = findViewById(R.id.etNumber);
+        EditText editNumberDecimal = findViewById(R.id.etNumberDecimal);
+        Switch aSwitch = findViewById(R.id.switch1);
+        Button btnActivity3 = findViewById(R.id.btnSendSecond);
+        Button btnBack = findViewById(R.id.btnBack);
+
+        // Cambiamos de plantilla
+        btnActivity3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentActivity = new Intent(SecondActivity.this, ThirdActivity.class);
+                intentActivity.putExtra("etText", editText.getText().toString());
+                intentActivity.putExtra("etNumber", editNumber.getText().toString());
+                intentActivity.putExtra("etNumberDecimal", editNumberDecimal.getText().toString());
+                intentActivity.putExtra("swich1", aSwitch.isChecked());
+                startActivity(intentActivity);
+            }
+        });
+
+        // Volvemos hacia atras
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentBack = new Intent(SecondActivity.this, MainActivity.class);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/myfirstappagc/ThirdActivity.java
+++ app/src/main/java/com/example/myfirstappagc/ThirdActivity.java
@@ -1,6 +1,10 @@
 package com.example.myfirstappagc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,33 @@ public class ThirdActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Obtenemos todos los componentes
+        TextView tvTexto = findViewById(R.id.tvText);
+        TextView tvNumber = findViewById(R.id.tvNumber);
+        TextView tvNumberDecimal = findViewById(R.id.tvNumberDecimal);
+        TextView tvSwich = findViewById(R.id.tvSwich);
+        Button btnBackToHome = findViewById(R.id.btnHome);
+
+
+        Intent intentSecondActivity = getIntent();
+        // Ponemos todos los datos correspondientes
+        tvTexto.setText(intentSecondActivity.getStringExtra("etText"));
+        tvNumber.setText(intentSecondActivity.getStringExtra("etNumber"));
+        tvNumberDecimal.setText(intentSecondActivity.getStringExtra("etNumberDecimal"));
+        // Obtenemos el valor del swich para cambiarle luego el tipo
+        Boolean bol = intentSecondActivity.getBooleanExtra("swich1",true);
+        // ahora si podemos poner el valor en el swich
+        tvSwich.setText(bol.toString());
+
+        btnBackToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentHome = new Intent(ThirdActivity.this, MainActivity.class);
+
+                startActivity(intentHome);
+            }
         });
     }
 }
